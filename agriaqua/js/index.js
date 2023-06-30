5e6ab93a281d50cf45336054e986df7ce91b4793
satyavenikaruprolu <satyakaruprolu129@gmail.com>
2023-06-30 20:46:55 +0530

    included all tabs in one file :wrench:

--- agriaqua/js/index.js
+++ agriaqua/js/index.js
@@ -1,12 +1,6 @@
-$(document).ready(function() {
-    // Load the default section on page load
-    $('#content').load('index.html');
-  
-    // Handle navigation clicks
-    $('.nav-link').click(function(event) {
-      event.preventDefault();
-      var section = $(this).attr('href');
-      $('#content').load(section + '.html');
-    });
-  });
-  
+function closeNavbar() {
+    var navbar = document.querySelector('.navbar-collapse');
+    if (navbar.classList.contains('show')) {
+    navbar.classList.remove('show');
+    }
+}
