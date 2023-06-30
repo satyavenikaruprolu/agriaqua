2e7dbe128a9dbacd66e8fac645e25beace422f50
narendra101 <narendra11470@gmail.com>
2023-07-01 00:45:08 +0530

    added new code

--- agriaqua/js/index.js
+++ agriaqua/js/index.js
@@ -1,6 +1,18 @@
-function closeNavbar() {
-    var navbar = document.querySelector('.navbar-collapse');
-    if (navbar.classList.contains('show')) {
-    navbar.classList.remove('show');
+const dropDownElement = document.getElementById('navbarNavDropdown');
+
+const navLinks = document.querySelectorAll('.nav-link');
+const dropdownItems = document.querySelectorAll('.dropdown-item');
+
+navLinks.forEach((link) => {
+    if(!link.textContent.includes("Explore More")) {
+        link.addEventListener('click', () => {
+            dropDownElement.classList.remove('show');
+        });
     }
-}
+})
+
+dropdownItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        dropDownElement.classList.remove('show');
+    })
+})
