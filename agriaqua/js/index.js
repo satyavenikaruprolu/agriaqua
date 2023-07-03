60833cd20c73df4574620954cb8cc302919d7a1a
narendra101 <narendra11470@gmail.com>
2023-07-03 11:22:04 +0530

    added some changes

--- agriaqua/js/index.js
+++ agriaqua/js/index.js
@@ -1,18 +1,4 @@
-const dropDownElement = document.getElementById('navbarNavDropdown');
-
-const navLinks = document.querySelectorAll('.nav-link');
-const dropdownItems = document.querySelectorAll('.dropdown-item');
-
-navLinks.forEach((link) => {
-    if(!link.textContent.includes("Explore More")) {
-        link.addEventListener('click', () => {
-            dropDownElement.classList.remove('show');
-        });
-    }
-})
-
-dropdownItems.forEach((item) => {
-    item.addEventListener('click', () => {
-        dropDownElement.classList.remove('show');
-    })
+const sections = []
+$(document).ready(function(){
+    
 })
