cd816d137c8ea62f6c7e96fc3d1112baedd2725f
narendra101 <narendra11470@gmail.com>
2023-07-04 10:59:37 +0530

    added new sections and fixed video rendering :wrench:

--- agriaqua/js/index.js
+++ agriaqua/js/index.js
@@ -1,4 +1,16 @@
-const sections = []
 $(document).ready(function(){
-    
+    // $('.nav-link').click(function(e){
+    //     if(!this.text.includes('Explore More')){
+    //         $('.nav-link').removeClass('active')
+    //         $(this).addClass('active')
+    //         // $($(this).attr('href')).addClass('active')
+    //         $($(this).attr('href')).show()
+    //         $('#navbarNavDropdown').removeClass('show')
+    //     }
+    // })
+
+    $('.dropdown-item').click(function(e){
+        $($(this).attr('href')).show()
+        $('#navbarNavDropdown').removeClass('show')
+    })
 })
