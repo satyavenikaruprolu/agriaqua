d74cb16d9a02a9543fa5d83bfaf03a277abdf7d2
narendra101 <narendra11470@gmail.com>
2023-07-21 18:37:19 +0530

    added new tabs and details :sparkles:

--- agriaqua/js/index.js
+++ agriaqua/js/index.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-    // $('.nav-link').click(function(e){
-    //     if(!this.text.includes('Explore More')){
-    //         $('.nav-link').removeClass('active')
-    //         $(this).addClass('active')
-    //         // $($(this).attr('href')).addClass('active')
-    //         $($(this).attr('href')).show()
-    //         $('#navbarNavDropdown').removeClass('show')
-    //     }
-    // })
-
     $('.dropdown-item').click(function(e){
         $($(this).attr('href')).show()
         $('#navbarNavDropdown').removeClass('show')
